87bce1a5662809e2f90783ee58000a8585fd82e1
Danny Lucas <lannyducas@gmail.com>
2022-08-24 02:13:44 -0400

    router links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, {useState} from 'react';
 import './App.css';
 import Menu from './components/Menu';
+import { 
+  Link,
+  Route,
+  Routes
+} from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PresetContainer from './components/PresetContainer'
+// import { PresetContainer } from './components/PresetContainer'
 import CurrentOrderContainer from './components/CurrentOrderContainer';
 import OrderHistoryContainer from './components/OrderHistoryContainer';
+import NewSandwichForm from './components/NewSandwichForm'
 
 function App() {
   const [currentOrders, setCurrentOrders] = useState([])
@@ -23,16 +31,33 @@ function App() {
   }
   
   return (
+
+
     <div className="App">
-      <Header/>
-      <h2>Pile It On, Have Fun!</h2>
-      <div className="Build Sandwich">
-      <PresetContainer onAddToOrder = {onAddToOrder}/>
-      {currentOrders.length!==0? <CurrentOrderContainer  onPlaceOrder = {onPlaceOrder} currentOrders = {currentOrders}/>:null}
-      </div>
-      <OrderHistoryContainer orderHistory = {orderHistory} />
-      <Footer/>
+         {/* <nav> 
+          <ul>
+            <li>
+          <Link to "/"> Home </Link>
+          </li>
+          </ul>
+          </nav>
+          */}
+        <Header /> 
+
+        <h2>Pile It On, Have Fun!</  h2>
+        <div className="Build Sandwich">
+        <PresetContainer onAddToOrder = {onAddToOrder}/>
+        {currentOrders.length!==0? <CurrentOrderContainer  onPlaceOrder = {onPlaceOrder} currentOrders = {currentOrders}/>:null}
+        </div>
+        <OrderHistoryContainer orderHistory = {orderHistory} />
+        <Footer/>
+      <Routes>
+        <Route path="/" component={<PresetContainer/> } />
+        <Route path = "/newsandwichform" component={<NewSandwichForm />} />
+      </Routes>
+
     </div>
+
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react"
 import LogIn from './LogIn'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return(
@@ -8,6 +9,10 @@ function Header() {
                 <img src="./images/Garbage-Sandwich.png" alt="Garbage-Sandwich"/>
                 <h1>Yummy! Build Your Garbage Sandwich</h1>
                 <LogIn />
+                
+                <Link to="/">Home</Link>
+                <Link to="/newsandwichform"> New Sandwich Form</Link>
+                
             </div>
         </header>
         
